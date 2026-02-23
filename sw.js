cff6ba9c4d71b3388b37a642fce5bebcb0b33bfb
adityarawat403-cpu <adityarawat403@gmail.com>
2026-02-23 16:08:07 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,42 @@
-const CACHE_NAME = "replyshastra-cache-v1";
+const CACHE_NAME = "replyshastra-v3";   // version change very important
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./icon.png",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener("install", e => {
+self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener("activate", e => {
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys.map(key => {
+if (key !== CACHE_NAME) {
+return caches.delete(key);
+}
+})
+)
+)
+);
+self.clients.claim();
+});
+
+// only cache images & audio (NOT index.html)
+self.addEventListener("fetch", e => {
+const url = new URL(e.request.url);
+
+// NEVER CACHE HTML
+if (url.pathname.endsWith(".html") || url.pathname === "/") {
+e.respondWith(fetch(e.request));
+return;
+}
+
+e.respondWith(
+caches.match(e.request).then(res => {
+return res || fetch(e.request).then(fetchRes => {
+return caches.open(CACHE_NAME).then(cache => {
+cache.put(e.request, fetchRes.clone());
+return fetchRes;
+});
+});
+})
+);
 });
